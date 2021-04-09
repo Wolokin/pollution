9351da0859ac8f477238609e211180f6d16b9c30
Wolokin <fnikol22@gmail.com>
2021-04-09 17:22:18 +0200

    Added API functions and changed loop function

--- src/pollution_server.erl
+++ src/pollution_server.erl
@@ -1,7 +1,7 @@
 -module(pollution_server).
 -author("filip").
 
--export([start/0, stop/0, call/1]).
+-export([start/0, stop/0, call/1, addStation/2, addValue/4, removeValue/3, getOneValue/3, getStationMean/2, getDailyMean/2, getDailyOverLimit/3, getClosestStation/1, changeStationName/2]).
 
 start() ->
   erlang:register(pollutionServer, spawn(fun init/0)).
@@ -20,43 +20,69 @@ call(Message) ->
   end.
 
 loop(Monitor) ->
-  receive
+  Result = receive
     {request, Pid, {addStation, {Name, Coords}}} ->
-      UpdatedMonitor = pollution:addStation(Name, Coords, Monitor),
-      Pid ! {reply, ok},
-      loop(UpdatedMonitor);
+      {ok, Pid, pollution:addStation(Name, Coords, Monitor)};
     {request, Pid, {addValue, {Station, Datetime, MeasurementType, Value}}} ->
-      UpdatedMonitor = pollution:addValue(Station, Datetime, MeasurementType, Value, Monitor),
-      Pid ! {reply, ok},
-      loop(UpdatedMonitor);
+      {ok, Pid, pollution:addValue(Station, Datetime, MeasurementType, Value, Monitor)};
     {request, Pid, {removeValue, {Station, Datetime, MeasurementType}}} ->
-      UpdatedMonitor = pollution:removeValue(Station, Datetime, MeasurementType, Monitor),
-      Pid ! {reply, ok},
-      loop(UpdatedMonitor);
+      {ok, Pid, pollution:removeValue(Station, Datetime, MeasurementType, Monitor)};
     {request, Pid, {getOneValue, {Station, Datetime, MeasurementType}}} ->
-      Value = pollution:getOneValue(Station, Datetime, MeasurementType, Monitor),
-      Pid ! {reply, Value},
-      loop(Monitor);
+      {pollution:getOneValue(Station, Datetime, MeasurementType, Monitor), Pid, Monitor};
     {request, Pid, {getStationMean, {Station, MeasurementType}}} ->
-      Value = pollution:getStationMean(Station, MeasurementType, Monitor),
-      Pid ! {reply, Value},
-      loop(Monitor);
+      {pollution:getStationMean(Station, MeasurementType, Monitor), Pid, Monitor};
     {request, Pid, {getDailyMean, {Date, MeasurementType}}} ->
-      Value = pollution:getDailyMean(Date, MeasurementType, Monitor),
-      Pid ! {reply, Value},
-      loop(Monitor);
+      {pollution:getDailyMean(Date, MeasurementType, Monitor), Pid, Monitor};
     {request, Pid, {getDailyOverLimit, {Date, MeasurementType, Norm}}} ->
-      Value = pollution:getDailyOverLimit(Date, MeasurementType, Norm, Monitor),
-      Pid ! {reply, Value},
-      loop(Monitor);
+      {pollution:getDailyOverLimit(Date, MeasurementType, Norm, Monitor), Pid, Monitor};
     {request, Pid, {getClosestStation, {Coords}}} ->
-      Value = pollution:getClosestStation(Coords, Monitor),
-      Pid ! {reply, Value},
-      loop(Monitor);
+      {pollution:getClosestStation(Coords, Monitor), Pid, Monitor};
     {request, Pid, {changeStationName, {OldName, NewName}}} ->
-      UpdatedMonitor = pollution:changeStationName(OldName, NewName, Monitor),
-      Pid ! {reply, ok},
-      loop(UpdatedMonitor);
+      {ok, Pid, pollution:changeStationName(OldName, NewName, Monitor)};
     {request, Pid, exit} ->
-      Pid ! {reply, ok}
+      {exit, Pid}
+  end,
+  case Result of
+    {exit, Pid} -> Pid ! {reply, ok};
+    {Value, Pid, Monitor} -> Pid ! {reply, Value},
+      loop(Monitor)
   end.
+
+% Dodaje nowa stacje pomiarowa
+addStation(Name, Coords) ->
+  call({addStation, {Name, Coords}}).
+
+% Dodaje pojedynczy pomiar o podanej wartosci do podanej stacji, daty i typu
+addValue(Station, Datetime, MeasurementType, Value) ->
+  call({addValue, {Station, Datetime, MeasurementType, Value}}).
+
+% Usuwa pojedynczy pomiar z podanej stacji, daty i typu
+removeValue(Station, Datetime, MeasurementType) ->
+  call({removeValue, {Station, Datetime, MeasurementType}}).
+
+% Zwraca wartosc pojedynczego pomiaru z podanej stacji, daty i typu
+getOneValue(Station, Datetime, MeasurementType) ->
+  call({getOneValue, {Station, Datetime, MeasurementType}}).
+
+% Zwraca srednia wartosc pomiaru danego typu dla danej stacji
+getStationMean(Station, MeasurementType) ->
+  call({getStationMean, {Station, MeasurementType}}).
+
+% Zwraca srednia wartosc danego parametru danego dnia na wszystkich stacjach
+getDailyMean(Date, MeasurementType) ->
+  call({getDailyMean, {Date, MeasurementType}}).
+
+% Dodatkowa funkcjonalnosc
+% Zwraca ilość stacji ktore danego dnia przekroczyly norme danego typu
+getDailyOverLimit(Date, MeasurementType, Norm) ->
+  call({getDailyOverLimit, {Date, MeasurementType, Norm}}).
+
+% Dodatkowa funkcjonalnosc
+% Zwraca nazwe najblizszej stacji w normie euklidesowej
+getClosestStation(Coords) ->
+  call({getClosestStation, {Coords}}).
+
+% Dodatkowa funkcjonalnosc
+% Zmienia nazwe stacji
+changeStationName(OldName, NewName) ->
+  call({changeStationName, {OldName, NewName}}).
--- tests/pollution_server_tests.erl
+++ tests/pollution_server_tests.erl
@@ -13,9 +13,9 @@
 
 initTest() ->
   pollution_server:start(),
-  pollution_server:call({addStation, {"Aleja Slowackiego", {50.2345, 18.3445}}}),
-  pollution_server:call({addValue, {{50.2345, 18.3445}, {{2021,3,22},{16,16,47}}, "PM10", 59}}),
-  pollution_server:call({addValue, {"Aleja Slowackiego", {{2021,3,22},{16,16,47}}, "PM2,5", 113}}).
+  pollution_server:addStation("Aleja Slowackiego", {50.2345, 18.3445}),
+  pollution_server:addValue({50.2345, 18.3445}, {{2021, 3, 22}, {16, 16, 47}}, "PM10", 59),
+  pollution_server:addValue("Aleja Slowackiego", {{2021, 3, 22}, {16, 16, 47}}, "PM2,5", 113).
 
 endTest() ->
   pollution_server:stop().
@@ -26,62 +26,62 @@ startStopServer_test() ->
 
 addStation_test() ->
   pollution_server:start(),
-  pollution_server:call({addStation, {"Aleja Slowackiego", {50.2345, 18.3445}}}),
+  pollution_server:addStation("Aleja Slowackiego", {50.2345, 18.3445}),
   pollution_server:stop().
 
 addValue_test() ->
   pollution_server:start(),
-  pollution_server:call({addStation, {"Aleja Slowackiego", {50.2345, 18.3445}}}),
-  pollution_server:call({addValue, {{50.2345, 18.3445}, {{2021,3,22},{16,16,47}}, "PM10", 59}}),
-  pollution_server:call({addValue, {"Aleja Slowackiego", {{2021,3,22},{16,16,47}}, "PM2,5", 113}}),
+  pollution_server:addStation("Aleja Slowackiego", {50.2345, 18.3445}),
+  pollution_server:addValue({50.2345, 18.3445}, {{2021, 3, 22}, {16, 16, 47}}, "PM10", 59),
+  pollution_server:addValue("Aleja Slowackiego", {{2021, 3, 22}, {16, 16, 47}}, "PM2,5", 113),
   pollution_server:stop().
 
 getOneValue_test() ->
   initTest(),
-  ?assert(59 == pollution_server:call({getOneValue, {"Aleja Slowackiego", {{2021,3,22},{16,16,47}}, "PM10"}})),
-  ?assert(113 == pollution_server:call({getOneValue, {{50.2345, 18.3445}, {{2021,3,22},{16,16,47}}, "PM2,5"}})),
+  ?assert(59 == pollution_server:getOneValue("Aleja Slowackiego", {{2021, 3, 22}, {16, 16, 47}}, "PM10")),
+  ?assert(113 == pollution_server:getOneValue({50.2345, 18.3445}, {{2021, 3, 22}, {16, 16, 47}}, "PM2,5")),
   endTest().
 
 removeValue_test() ->
   initTest(),
-  pollution_server:call({removeValue, {"Aleja Slowackiego", {{2021,3,22},{16,16,47}}, "PM10"}}),
+  pollution_server:removeValue("Aleja Slowackiego", {{2021, 3, 22}, {16, 16, 47}}, "PM10"),
   %% Nie wiem jak assertowac bledy w innych watkach
-  % ?assertError({badkey, _}, pollution_server:call({getOneValue, {"Aleja Slowackiego", {{2021,3,22},{16,16,47}}, "PM10"}})),
+  % ?assertError({badkey, _}, pollution_server:getOneValue("Aleja Slowackiego", {{2021,3,22},{16,16,47}}, "PM10")),
   endTest().
 
 getStationMean_test() ->
   initTest(),
-  ?assertEqual(59.0, pollution_server:call({getStationMean, {"Aleja Slowackiego", "PM10"}})),
-  pollution_server:call({addValue, {"Aleja Slowackiego", {{2021,3,23},{16,16,47}}, "PM10", 61}}),
-  ?assertEqual(60.0, pollution_server:call({getStationMean, {"Aleja Slowackiego", "PM10"}})),
+  ?assertEqual(59.0, pollution_server:getStationMean("Aleja Slowackiego", "PM10")),
+  pollution_server:addValue("Aleja Slowackiego", {{2021, 3, 23}, {16, 16, 47}}, "PM10", 61),
+  ?assertEqual(60.0, pollution_server:getStationMean("Aleja Slowackiego", "PM10")),
   endTest().
 
 getDailyMean_test() ->
   initTest(),
-  ?assertEqual(59.0, pollution_server:call({getDailyMean, {{2021,3,22}, "PM10"}})),
-  pollution_server:call({addStation,{"Stacja 2", {12,34}}}),
-  pollution_server:call({addValue,{"Stacja 2", {{2021,3,22},{16,16,47}}, "PM5", 61}}),
-  ?assertEqual(59.0, pollution_server:call({getDailyMean,{{2021,3,22}, "PM10"}})),
-  pollution_server:call({addValue,{"Stacja 2", {{2021,3,22},{16,16,47}}, "PM10", 61}}),
-  ?assertEqual(60.0, pollution_server:call({getDailyMean,{{2021,3,22}, "PM10"}})),
+  ?assertEqual(59.0, pollution_server:getDailyMean({2021, 3, 22}, "PM10")),
+  pollution_server:addStation("Stacja 2", {12, 34}),
+  pollution_server:addValue("Stacja 2", {{2021, 3, 22}, {16, 16, 47}}, "PM5", 61),
+  ?assertEqual(59.0, pollution_server:getDailyMean({2021, 3, 22}, "PM10")),
+  pollution_server:addValue("Stacja 2", {{2021, 3, 22}, {16, 16, 47}}, "PM10", 61),
+  ?assertEqual(60.0, pollution_server:getDailyMean({2021, 3, 22}, "PM10")),
   endTest().
 
 getClosestStation_test() ->
   initTest(),
-  ?assertEqual("Aleja Slowackiego", pollution_server:call({getClosestStation,{{10,10}}})),
-  pollution_server:call({addStation,{"Stacja 2", {11,11}}}),
-  ?assertEqual("Stacja 2", pollution_server:call({getClosestStation,{{10,10}}})),
-  ?assertEqual("Aleja Slowackiego", pollution_server:call({getClosestStation,{{50.2345, 18.3445}}})),
+  ?assertEqual("Aleja Slowackiego", pollution_server:getClosestStation({10, 10})),
+  pollution_server:addStation("Stacja 2", {11, 11}),
+  ?assertEqual("Stacja 2", pollution_server:getClosestStation({10, 10})),
+  ?assertEqual("Aleja Slowackiego", pollution_server:getClosestStation({50.2345, 18.3445})),
   endTest().
 
 getDailyOverLimit_test() ->
   initTest(),
-  ?assertEqual(1, pollution_server:call({getDailyOverLimit,{{2021,3,22}, "PM10", 40}})),
-  ?assertEqual(0, pollution_server:call({getDailyOverLimit,{{2021,3,22}, "PM10", 60}})),
+  ?assertEqual(1, pollution_server:getDailyOverLimit({2021, 3, 22}, "PM10", 40)),
+  ?assertEqual(0, pollution_server:getDailyOverLimit({2021, 3, 22}, "PM10", 60)),
   endTest().
 
 changeStationName_test() ->
   initTest(),
-  pollution_server:call({changeStationName,{"Aleja Slowackiego", "Stacja"}}),
-  ?assertEqual(59, pollution_server:call({getOneValue,{"Stacja", {{2021,3,22},{16,16,47}}, "PM10"}})),
+  pollution_server:changeStationName("Aleja Slowackiego", "Stacja"),
+  ?assertEqual(59, pollution_server:getOneValue("Stacja", {{2021, 3, 22}, {16, 16, 47}}, "PM10")),
   endTest().
